360e0842072bf4e1606c5df6573542af2d1b9d57
GICHTENAERE-Evan <evan.gichtenaere@edu.univ-fcomte.fr>
2026-01-05 11:04:02 +0100

    header

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 
 À compléter :
 
-- NOM :
-- PRÉNOM :
-- GROUPE :
+- NOM :GICHTENAERE
+- PRÉNOM :Evan
+- GROUPE :A1
 - URL du site :
-- URL FIGMA :
+- URL FIGMA :https://www.figma.com/design/0hAaWp4Cd1YOR35pqy9uRr/Maquettes-projet-individuel-%E2%80%94-Evan-GICHTENAERE-A1?node-id=704-113&t=1cHA9P38Mqb52yEo-1
+
---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+<script>
+  const menuBtn = document.querySelector('.menu-btn');
+  const menu = document.querySelector('.menu');
+
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('open');
+  });
+</script>
+
+/*menu ouvert */
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('open');
+});
+
+
+
+
+
